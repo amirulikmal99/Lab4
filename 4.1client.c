4cd5fa0c99a4535c17654a26a2a98b3e1075a051
Amirul Ikmal <amirul.ikmal88@gmail.com>
2021-05-08 07:15:22 +0000

    lab4 commit

--- 4.1client.c
+++ 4.1client.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>	//inet_addr
+#include<string.h>
 
 int main(int argc , char *argv[])
 {
 	int socket_desc;
-	struct sockaddr_in server;
+	struct sockaddr_in server; 
+              char *message;
 	
 	//Create socket
 	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
@@ -14,7 +16,7 @@ int main(int argc , char *argv[])
 		printf("Could not create socket");
 	}
 		
-	server.sin_addr.s_addr = inet_addr("192.168.56.102");
+	server.sin_addr.s_addr = inet_addr("192.168.56.103"); 
 	server.sin_family = AF_INET;
 	server.sin_port = htons(22);
 
@@ -25,6 +27,15 @@ int main(int argc , char *argv[])
 		return 1;
 	}
 	
-	puts("Connected");
+	puts("Connected \n");
+
+	//Send some data
+	message = "connect";
+	if( send(socket_desc , message , strlen(message) , 0) < 0)
+	{
+		puts("Send failed");
+		return 1;
+	}
+	puts("Data Send\n");
 	return 0;
 }
